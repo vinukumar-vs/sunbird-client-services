4636b760e56d8e546c82368662d724680c6f0a84
devarfath <dev.arfath@gmail.com>
2019-12-30 17:57:54 +0530

    "RELEASE"

---
+++ dist/user/index.d.ts
@@ -0,0 +1,20 @@
+export interface User {
+    userId: string;
+    identifier: string;
+    firstName: string;
+    lastName?: string;
+    rootOrg: RootOrg;
+    tncAcceptedVersion: string;
+    tncAcceptedOn: string;
+    tncLatestVersion: string;
+    promptTnC: boolean;
+    tncLatestVersionUrl: string;
+    id: string;
+    avatar: string;
+}
+export interface RootOrg {
+    rootOrgId?: string;
+    orgName?: string;
+    slug?: string;
+    hashTagId: string;
+}
