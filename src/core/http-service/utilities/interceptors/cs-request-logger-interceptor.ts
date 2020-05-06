6085465a6268db7fa8a9eae1e4e1586ddbdc5bff
devarfath <dev.arfath@gmail.com>
2020-05-06 12:20:08 +0530

    Issue: #SH-22 chore: ADDED: interceptor logger utilities

---
+++ src/core/http-service/utilities/interceptors/cs-request-logger-interceptor.ts
@@ -0,0 +1,10 @@
+import {CsRequestInterceptor} from '../../interface/cs-request-interceptor';
+import {CsRequest} from '../../interface/cs-request';
+import {Observable, of} from 'rxjs';
+
+export class CsRequestLoggerInterceptor implements CsRequestInterceptor {
+    interceptRequest(request: CsRequest): Observable<CsRequest> {
+        console.log('CsRequestLoggerInterceptor: ', 'request:', JSON.stringify(request));
+        return of(request);
+    }
+}
