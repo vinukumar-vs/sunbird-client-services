eb1b453cd922d81c82bb61a1275073c053c43fd5
swayangjit <swayangjit029@gmail.com>
2020-09-01 15:58:23 +0530

    Issue #SH-1031 feat: Added contentType and primary category constants.

---
+++ src/services/content/interface/cs-primary-category.ts
@@ -0,0 +1,12 @@
+export enum CsPrimaryCategory {
+    EXPLANATION_CONTENT = 'ExplanationContent',
+    LEARNING_RESOURCE = 'LearningResource',
+    ONLINE_COURSE = 'OnlineCourse',
+    QUESTION_SET = 'QuestionSet',
+    ETEXTBOOK = 'eTextBook',
+    TEACHER_RESOURCE = 'TeacherResource',
+    COURSE_ASSESMENT = 'CourseAssessment',
+    ETB = 'ETB',
+    CONTENT_PLAYLIST = 'ContentPlayList',
+    GENERIC_COLLECTION = 'GenericCollection'
+}
--- src/services/content/interface/index.ts
+++ src/services/content/interface/index.ts
@@ -1,2 +1,4 @@
 export * from './cs-content-sort-criteria';
 export * from './cs-content-mime-type';
+export * from './cs-content-type';
+export * from './cs-primary-category';
