38c523e9f63d4b1230652ca892b362f7088e4788
devarfath <dev.arfath@gmail.com>
2020-09-11 17:53:21 +0530

    Issue #000 feat: Migrated from uglifyjs to terser for minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const config = {
     entry: {
@@ -55,8 +55,9 @@ const config = {
         extensions: ['.tsx', '.ts', '.js']
     },
     optimization: {
-        minimizer: [new UglifyJsPlugin({
-            sourceMap: true
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            sourceMap: true,
         })],
     },
     performance: {
