8ec19ceec4e0c17c92b668d7b6561add869a342b
JSDevL <dev.arfath@gmail.com>
2020-11-24 15:24:49 +0530

    Issue #SB-20744 chore: ADDED: CsCourseService.getContentState()

--- src/services/course/interface/cs-course-service.ts
+++ src/services/course/interface/cs-course-service.ts
@@ -16,8 +16,45 @@ export interface CertificateUrlResponse {
     printUri: string;
 }
 
+export interface GetContentStateRequest {
+    userId: string;
+    courseId: string;
+    batchId: string;
+    contentIds: string[];
+    fields?: ('progress' | 'score')[];
+}
+
+export enum ContentStateStatus {
+    IN_PROGRESS = 1,
+    COMPLETED = 2
+}
+
+export interface ContentState {
+    id: string;
+    contentVersion: string;
+    userId: string;
+    lastAccessTime: string;
+    contentId: string;
+    progress: number;
+    viewCount: number;
+    completedCount: number;
+    batchId: string;
+    courseId: string;
+    collectionId: string;
+    lastCompletedTime: string;
+    status: ContentStateStatus;
+    score?: {
+        attemptId: string;
+        lastAttemptedOn: string,
+        totalMaxScore: number;
+        totalScore: number;
+    }[];
+}
+
 export interface CsCourseService {
     getUserEnrolledCourses(request: GetUserEnrolledCoursesRequest, additionalParams?: { [key: string]: string }, config?: CsCourseServiceConfig): Observable<Course[]>;
 
     getSignedCourseCertificate(certificateId: string, config?: CsCourseServiceConfig): Observable<CertificateUrlResponse>;
+
+    getContentState(request: GetContentStateRequest, config?: CsCourseServiceConfig): Observable<ContentState[]>;
 }
