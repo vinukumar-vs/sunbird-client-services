61ddaa0f4013bbf50c2d6949ecbc0b7fb214c636
Balakrishna M <balakrishna@mantralabsglobal.com>
2021-09-17 09:15:05 +0530

    Issue #SB-24362 feat: Notification test cases added

--- src/models/notification/index.ts
+++ src/models/notification/index.ts
@@ -19,20 +19,20 @@ export interface NotificationTemplate {
 }
 
 export interface NotificationAction {
-    type: NotificationType | any,
-    category: NotificationCategory | any,
-    template: NotificationTemplate,
-    createdBy: any,
-    additionalInfo: any,
-    groupRole: string
+    type?: NotificationType | any,
+    category?: NotificationCategory | any,
+    template?: NotificationTemplate,
+    createdBy?: any,
+    additionalInfo?: any,
+    groupRole?: string
 }
 
 export interface NotificationData {
     id: string,
     userId?: string,                    
     priority?: number,
-    createdBy: string,
-    status:NotificationStatus | any,
-    createdOn: number,
-    action: NotificationAction                                     
+    createdBy?: string,
+    status?:NotificationStatus | any,
+    createdOn?: number,
+    action?: NotificationAction                                     
 }
