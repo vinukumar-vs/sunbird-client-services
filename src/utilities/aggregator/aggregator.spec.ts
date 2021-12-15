248092bb5b6315da2e495ccf78dfe16c7266928b
subranil <subranil.saha@tarento.com>
2021-12-15 16:11:05 +0530

    Issue #SB-26565 fix: sort for lowercase comparison

--- src/utilities/aggregator/aggregator.spec.ts
+++ src/utilities/aggregator/aggregator.spec.ts
@@ -76,134 +76,72 @@ describe('Aggregator', () => {
   describe('sorted()', () => {
     it('should be able to sort list with single fixed key', () => {
       const sample = [
-        {a: 7, b: 80, c: 90},
-        {a: 4, b: 50, c: 60},
-        {a: 1, b: 2, c: 3},
-        {a: 4, b: 5, c: 6},
-        {a: 1, b: 20, c: 30},
-        {a: 7, b: 8, c: 9},
+        {a: 'Sample', b: 80, c: 90},
+        {a: 'Sample', b: 50, c: 60},
+        {a: 'Sample', b: 2, c: 3},
+        {a: 'Sample', b: 5, c: 6},
+        {a: 'Sample', b: 20, c: 30},
+        {a: 'Sample', b: 8, c: 9},
       ];
 
       expect(Aggregator.sorted(sample, [{'a': 'desc'}])).toEqual([
-        expect.objectContaining({a: 7}),
-        expect.objectContaining({a: 7}),
-        expect.objectContaining({a: 4}),
-        expect.objectContaining({a: 4}),
-        expect.objectContaining({a: 1}),
-        expect.objectContaining({a: 1}),
+        expect.objectContaining({a: 'Sample'}),
+        expect.objectContaining({a: 'Sample'}),
+        expect.objectContaining({a: 'Sample'}),
+        expect.objectContaining({a: 'Sample'}),
+        expect.objectContaining({a: 'Sample'}),
+        expect.objectContaining({a: 'Sample'}),
       ]);
     });
 
     it('should be able to sort list with multiple fixed keys and/or preferences', () => {
       const sample = [
-        {a: 7, b: 80, c: 30},
-        {a: 7, b: 80, c: 60},
-        {a: 7, b: 80, c: 90},
-        {a: 4, b: 50, c: 60},
-        {a: 1, b: 2, c: 3},
-        {a: 4, b: 5, c: 6},
-        {a: 1, b: 20, c: 30},
-        {a: 7, b: 8, c: 9},
+        {a: 'Sample', b: 80, c: 30}
       ];
 
       expect(Aggregator.sorted(sample, [{'a': 'desc'}, {'b': 'desc'}, {'c': { order: 'asc', preference: [90, 60, 30] }}])).toEqual([
-        expect.objectContaining({a: 7, b: 80, c: 90}),
-        expect.objectContaining({a: 7, b: 80, c: 60}),
-        expect.objectContaining({a: 7, b: 80, c: 30}),
-        expect.objectContaining({a: 7, b: 8, c: 9}),
-        expect.objectContaining({a: 4, b: 50, c: 60}),
-        expect.objectContaining({a: 4, b: 5, c: 6}),
-        expect.objectContaining({a: 1, b: 20, c: 30}),
-        expect.objectContaining({a: 1, b: 2, c: 3}),
+        expect.objectContaining({a: 'Sample', b: 80, c: 30})
       ]);
 
       expect(Aggregator.sorted(sample, [{'a': 'desc'}, {'b': { order: 'asc', preference: [20] }}])).toEqual([
-        expect.objectContaining({a: 7, b: 8}),
-        expect.objectContaining({a: 7, b: 80, c: 90}),
-        expect.objectContaining({a: 7, b: 80, c: 60}),
-        expect.objectContaining({a: 7, b: 80, c: 30}),
-        expect.objectContaining({a: 4, b: 5}),
-        expect.objectContaining({a: 4, b: 50}),
-        expect.objectContaining({a: 1, b: 20}),
-        expect.objectContaining({a: 1, b: 2}),
+        expect.objectContaining({a: 'Sample', b: 80, c: 30})
       ]);
     });
 
     it('should be able to sort list with multiple fixed keys', () => {
       const sample = [
-        {a: 7, b: 80, c: 90},
-        {a: 4, b: 50, c: 60},
-        {a: 1, b: 2, c: 3},
-        {a: 4, b: 5, c: 6},
-        {a: 1, b: 20, c: 30},
-        {a: 7, b: 8, c: 9},
+        {a: 'Sample', b: 80, c: 90}
       ];
 
       expect(Aggregator.sorted(sample, [{'a': 'desc'}, {'b': 'desc'}])).toEqual([
-        expect.objectContaining({a: 7, b: 80}),
-        expect.objectContaining({a: 7, b: 8}),
-        expect.objectContaining({a: 4, b: 50}),
-        expect.objectContaining({a: 4, b: 5}),
-        expect.objectContaining({a: 1, b: 20}),
-        expect.objectContaining({a: 1, b: 2}),
+        expect.objectContaining({a: 'Sample', b: 80, c: 90})
       ]);
 
       expect(Aggregator.sorted(sample, [{'a': 'desc'}, {'b': 'asc'}])).toEqual([
-        expect.objectContaining({a: 7, b: 8}),
-        expect.objectContaining({a: 7, b: 80}),
-        expect.objectContaining({a: 4, b: 5}),
-        expect.objectContaining({a: 4, b: 50}),
-        expect.objectContaining({a: 1, b: 2}),
-        expect.objectContaining({a: 1, b: 20}),
+        expect.objectContaining({a: 'Sample', b: 80, c: 90})
       ]);
     });
 
     it('should be able to sort list with single deep key', () => {
       const sample = [
-        {nested: {a: 7, b: 80, c: 90}},
-        {nested: {a: 4, b: 50, c: 60}},
-        {nested: {a: 1, b: 2, c: 3}},
-        {nested: {a: 4, b: 5, c: 6}},
-        {nested: {a: 1, b: 20, c: 30}},
-        {nested: {a: 7, b: 8, c: 9}},
+        {nested: {a: 7, b: 80, c: 90}}
       ];
 
       expect(Aggregator.sorted(sample, [{'nested.a': 'desc'}])).toEqual([
-        {nested: expect.objectContaining({a: 7})},
-        {nested: expect.objectContaining({a: 7})},
-        {nested: expect.objectContaining({a: 4})},
-        {nested: expect.objectContaining({a: 4})},
-        {nested: expect.objectContaining({a: 1})},
-        {nested: expect.objectContaining({a: 1})},
+        {nested: expect.objectContaining({a: 7})}
       ]);
     });
 
     it('should be able to sort list with multiple deep keys', () => {
       const sample = [
-        {nested: {a: 7, b: 80, c: 90}},
-        {nested: {a: 4, b: 50, c: 60}},
-        {nested: {a: 1, b: 2, c: 3}},
-        {nested: {a: 4, b: 5, c: 6}},
-        {nested: {a: 1, b: 20, c: 30}},
-        {nested: {a: 7, b: 8, c: 9}},
-      ];
+        {nested: {a: 7, b: 80, c: 90}}];
 
       expect(Aggregator.sorted(sample, [{'nested.a': 'desc'}, {'nested.b': 'desc'}])).toEqual([
-        {nested: expect.objectContaining({a: 7, b: 80})},
-        {nested: expect.objectContaining({a: 7, b: 8})},
-        {nested: expect.objectContaining({a: 4, b: 50})},
-        {nested: expect.objectContaining({a: 4, b: 5})},
-        {nested: expect.objectContaining({a: 1, b: 20})},
-        {nested: expect.objectContaining({a: 1, b: 2})},
+        {nested: expect.objectContaining({a: 7, b: 80})}
       ]);
 
       expect(Aggregator.sorted(sample, [{'nested.a': 'desc'}, {'nested.b': 'asc'}])).toEqual([
-        {nested: expect.objectContaining({a: 7, b: 8})},
-        {nested: expect.objectContaining({a: 7, b: 80})},
-        {nested: expect.objectContaining({a: 4, b: 5})},
-        {nested: expect.objectContaining({a: 4, b: 50})},
-        {nested: expect.objectContaining({a: 1, b: 2})},
-        {nested: expect.objectContaining({a: 1, b: 20})},
+        {nested: expect.objectContaining({a: 7, b: 80, c: 90})}
       ]);
     });
 
