2fa3c25b09c046c0ba66afc450dfa5adda08ab64
swayangjit <swayangjit029@gmail.com>
2022-03-15 19:55:25 +0530

    Issue #SB-29117 feat: Updated .getPublicKey method

--- src/services/certificate/implementation/certificate-service-impl.ts
+++ src/services/certificate/implementation/certificate-service-impl.ts
@@ -27,127 +27,138 @@ export class CertificateServiceImpl implements CsCertificateService {
   ) {
   }
 
-    fetchCertificatesV1(request: CSGetLearnerCerificateRequest, config?: CsCertificateServiceConfig): Observable<CsLearnerCertificateV1[]> {
-        if (!this.apiPath && (!config || !config.apiPath)) {
-            throw new Error('Required certificate api Path configuration');
-        }
+  fetchCertificatesV1(request: CSGetLearnerCerificateRequest, config?: CsCertificateServiceConfig): Observable<CsLearnerCertificateV1[]> {
+    if (!this.apiPath && (!config || !config.apiPath)) {
+      throw new Error('Required certificate api Path configuration');
+    }
 
-        let certRequest = {
-            ...(request.size ? { size: request.size } : null),
-            _source: [
-              'data.badge.issuer.name',
-              'pdfUrl',
-              'data.issuedOn',
-              'data.badge.name',
-              'related.courseId',
-              'related.Id'
-            ],
-            query: {
-              bool: {
-                must: [
-                  {
-                    match_phrase: {
-                      'recipient.id': request.userId
-                    }
-                  }
-                ]
+    let certRequest = {
+      ...(request.size ? { size: request.size } : null),
+      _source: [
+        'data.badge.issuer.name',
+        'pdfUrl',
+        'data.issuedOn',
+        'data.badge.name',
+        'related.courseId',
+        'related.Id'
+      ],
+      query: {
+        bool: {
+          must: [
+            {
+              match_phrase: {
+                'recipient.id': request.userId
               }
             }
-          };
-
-        const apiRequest: CsRequest = new CsRequest.Builder()
-            .withType(CsHttpRequestType.POST)
-            .withPath(`${config ? config.apiPath : this.apiPath}/certs/search`)
-            .withBearerToken(true)
-            .withUserToken(true)
-            .withBody({
-                request: certRequest
-            })
-            .build();
-
-        return this.httpService.fetch<{ result: { response: { count: number, content: CsLearnerCertificateV1[] } } }>(apiRequest)
-        .pipe(
-            map((response) => {
-            return response.body.result.response.content;
-            })
-        );
-    }
-
-    fetchCertificatesV2(request: CSGetLearnerCerificateRequest, config?: CsCertificateServiceConfig): Observable<CsLearnerCertificate[]> {
-        if (!this.rcApiPath && (!config || !config.rcApiPath)) {
-            throw new Error('Required certificate api Path configuration');
+          ]
         }
+      }
+    };
 
-        let certRequest = {
-            filters:{
-                contact:{
-                    eq: request.userId
-                }
-            }
-        };
-
-        const apiRequest: CsRequest = new CsRequest.Builder()
-            .withType(CsHttpRequestType.POST)
-            .withPath(`${config ? config.rcApiPath : this.rcApiPath}/search`)
-            .withBearerToken(true)
-            .withUserToken(true)
-            .withBody({
-                request: certRequest
-            })
-            .build();
-
-        return this.httpService.fetch<{ result: CsLearnerCertificateV2[] }>(apiRequest)
-        .pipe(
-            map((response) => {
-            return response.body.result.map(r => {
-                let result = {
-                    id: r.training.id,
-                    name: r.training.name,
-                    issuerName: r.issuer.name,
-                }
-                return result;
-            });
-            })
-        );
-    }
-
-    fetchcertificates(request: CSGetLearnerCerificateRequest): Observable<CsLearnerCertificate[]> {
-        return this.fetchCertificatesV1(request).pipe(
-            map((r) => r.map((rs) => {
-                let result = {
-                    id: rs._id,
-                    courseId: rs._source.related.courseId,
-                    name: rs._source.data.badge.name,
-                    pdfUrl: rs._source.pdfUrl,
-                    issuedOn: rs._source.data.issuedOn,
-                    issuerName: rs._source.data.badge.issuer.name,
-                }
-                return result;
-            })
-            ),
-            mergeMap((result) => {
-                return this.fetchCertificatesV2(request).pipe(
-                    map(r => [...result, ...r])
-                )
-            })  
-        )
-    }
-
-  getPublicKey(request: GetPublicKeyRequest, config?: CsCertificateServiceConfig): Observable<GetPublicKeyResponse> {
     const apiRequest: CsRequest = new CsRequest.Builder()
       .withType(CsHttpRequestType.POST)
-      .withPath((config ? config.rcApiPath : this.rcApiPath) + `/${request.signingKey}`)
+      .withPath(`${config ? config.apiPath : this.apiPath}/certs/search`)
       .withBearerToken(true)
       .withUserToken(true)
-      .withBody({ request })
+      .withBody({
+        request: certRequest
+      })
       .build();
 
-    return this.httpService.fetch<{ result: { SigningKey: GetPublicKeyResponse } }>(apiRequest)
+    return this.httpService.fetch<{ result: { response: { count: number, content: CsLearnerCertificateV1[] } } }>(apiRequest)
       .pipe(
         map((response) => {
-          return response.body.result.SigningKey;
+          return response.body.result.response.content;
         })
       );
+  }
+
+  fetchCertificatesV2(request: CSGetLearnerCerificateRequest, config?: CsCertificateServiceConfig): Observable<CsLearnerCertificate[]> {
+    if (!this.rcApiPath && (!config || !config.rcApiPath)) {
+      throw new Error('Required certificate api Path configuration');
+    }
+
+    let certRequest = {
+      filters: {
+        contact: {
+          eq: request.userId
+        }
+      }
+    };
+
+    const apiRequest: CsRequest = new CsRequest.Builder()
+      .withType(CsHttpRequestType.POST)
+      .withPath(`${config ? config.rcApiPath : this.rcApiPath}/search`)
+      .withBearerToken(true)
+      .withUserToken(true)
+      .withBody({
+        request: certRequest
+      })
+      .build();
+
+    return this.httpService.fetch<{ result: CsLearnerCertificateV2[] }>(apiRequest)
+      .pipe(
+        map((response) => {
+          return response.body.result.map(r => {
+            let result = {
+              id: r.training.id,
+              name: r.training.name,
+              issuerName: r.issuer.name,
+            }
+            return result;
+          });
+        })
+      );
+  }
+
+  fetchCertificates(request: CSGetLearnerCerificateRequest): Observable<CsLearnerCertificate[]> {
+    return this.fetchCertificatesV1(request).pipe(
+      map((r) => r.map((rs) => {
+        let result = {
+          id: rs._id,
+          courseId: rs._source.related.courseId,
+          name: rs._source.data.badge.name,
+          pdfUrl: rs._source.pdfUrl,
+          issuedOn: rs._source.data.issuedOn,
+          issuerName: rs._source.data.badge.issuer.name,
+        }
+        return result;
+      })
+      ),
+      mergeMap((result) => {
+        return this.fetchCertificatesV2(request).pipe(
+          map(r => [...result, ...r])
+        )
+      })
+    )
+  }
+
+  getPublicKey(request: GetPublicKeyRequest, config?: CsCertificateServiceConfig): Observable<GetPublicKeyResponse> {
+    return defer(async () => {
+      let schemaName = request.schemaName
+      if (!schemaName) {
+        try {
+          schemaName = await this.systemSettingsService.getSystemSettings("certificate_schema").toPromise();
+        } catch (e) {
+          throw new Error('Schema Name Not found');
+        }
+      }
+
+      const apiRequest: CsRequest = new CsRequest.Builder()
+        .withType(CsHttpRequestType.POST)
+        .withPath((config ? config.rcApiPath : this.rcApiPath).replace("${schemaName}", schemaName) + `/key/${request.osid}`)
+        .withBearerToken(true)
+        .withUserToken(true)
+        .withBody({ request })
+        .build();
+
+      return this.httpService.fetch<GetPublicKeyResponse>(apiRequest)
+        .pipe(
+          map((response) => {
+            return response.body;
+          })
+        ).toPromise();
+    });
   }
 
   getCerificateDownloadURI(request: FetchCertificateRequest, config?: CsCertificateServiceConfig): Observable<FetchCertificateResponse> {
@@ -158,7 +169,7 @@ export class CertificateServiceImpl implements CsCertificateService {
         if (!schemaName) {
           try {
             schemaName = await this.systemSettingsService.getSystemSettings("certificate_schema").toPromise();
-          } catch(e){
+          } catch (e) {
             throw new Error('Schema Name Not found');
           }
         }
