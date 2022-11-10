566b28842ab3cc2c86d91018810fa08444b9a597
Pavithra Prakash <pavithra.prakash@tarento.com>
2022-11-10 14:24:38 +0530

    Issue #SB-31126 fix:sort descending order for groups created

--- src/services/group/implementation/group-service-impl.ts
+++ src/services/group/implementation/group-service-impl.ts
@@ -33,6 +33,7 @@ import {map, mergeMap} from 'rxjs/operators';
 import {CsGroupActivityService} from '../activity/interface';
 import {CsFormService} from '../../form/interface/cs-form-service';
 import {Form} from '../../../models/form';
+import * as moment from 'moment';
 
 @injectable()
 export class GroupServiceImpl implements CsGroupService {
@@ -326,7 +327,7 @@ export class GroupServiceImpl implements CsGroupService {
                         return -1;
                     }
 
-                    return new Date(bLastActivity).getTime() - new Date(aLastActivity).getTime();
+                    return moment(bLastActivity, moment.defaultFormat).valueOf() - moment(aLastActivity, moment.defaultFormat).valueOf()
                 })
                     .map((g) => CsGroup.fromJSON(g) as CsGroupSearchResponse)
             )
